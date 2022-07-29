34cc2c2dc6b54354af02c6f143f9bb7addcab35e
André Nunes <andre.n@edu.sc.senai.br>
2022-07-28 22:03:46 -0300

    aula 4 atualizada

--- estruturas-dados/src/aula4/BinarySearchAula4.java
+++ estruturas-dados/src/aula4/BinarySearchAula4.java
@@ -12,16 +12,16 @@ public class BinarySearchAula4 {
         lista.add(11); // adiciona item a lista
         lista.add(10); //adionando item fora de ordem
 
-        if(Collections.binarySearch(lista,10) <= -1){
-            Collections.sort(lista); // realiza a organização da lista em ordem do menor para o maior
-            if(Collections.binarySearch(lista,10) <= -1)
+        if(Collections.binarySearch(lista,10) <= -1){ // se o valor não existe ou está fora de ordem ele retorna um numero negativo
+            Collections.sort(lista);                    // realiza a organização da lista em ordem do menor para o maior
+            if(Collections.binarySearch(lista,10) <= -1)  // se o valor não existe ou está fora de ordem ele retorna um numero negativo
                 System.out.println("Valor 10 não encontrado na lista");
             else
                 System.out.println("Valor 10 encontrado na lista");
         }
 
         System.out.println("Resultado de busca binária: "
-                +Collections.binarySearch(lista, 7)); //procurando na lista o valor 1 com o algoritimo binary search
+                +Collections.binarySearch(lista, 7)); // retorna o endereço do valor 7 na lista "lista"
         // se o binary search não encontra o valor ele retorna um valor negativo
         // ((lista.size()) + 1) *(-1),
     }
